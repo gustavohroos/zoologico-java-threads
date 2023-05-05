0bd788b60b84a944265b081f77f87e4514040af9
Gustavo Roos <gustavohroos@gmail.com>
2023-05-05 02:19:22 -0300

    abstração do método run, adição do atributo cor

--- Animal.java
+++ Animal.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-class Animal implements Runnable {
+abstract class Animal implements Runnable {
     String especie;
     int id;
     int tempoDeSonoMin;
@@ -10,6 +10,7 @@ class Animal implements Runnable {
     int comidaConsumida;
     int horasDormidas;
     Comedouro comedouro;
+    String cor;
     Random rand = new Random();
 
     Animal(Comedouro comedouro, int id) {
@@ -18,7 +19,38 @@ class Animal implements Runnable {
     }
 
     public void run() {
+        while(true){
+            try {
+                // O animal começa a comer
+                int comidaConsumidaNessaRefeicao = rand.nextInt(this.consomeMax - this.consomeMin + 1) + this.consomeMin;
+                System.out.println(this.cor + this.especie + " " + this.id + " quer comer " + comidaConsumidaNessaRefeicao + " unidades de comida." + "\u001B[0m");
+                comedouro.comer(this, comidaConsumidaNessaRefeicao);
+                Thread.yield();
 
+                // O animal para de comer e se exibe
+                System.out.println(this.cor  + this.especie + " " + this.id + " terminou de comer " + comidaConsumidaNessaRefeicao + " unidades de comida." + "\u001B[0m");
+                this.comidaConsumida += comidaConsumidaNessaRefeicao;
+                System.out.println(this.cor  + this.especie + " " + this.id + " começou a se exibir." + "\u001B[0m");
+
+                // O animal começa a comer de novo
+                comidaConsumidaNessaRefeicao = rand.nextInt(this.consomeMax - this.consomeMin + 1) + this.consomeMin;
+                this.comidaConsumida += comidaConsumidaNessaRefeicao;
+                System.out.println(this.cor  + this.especie + " " + this.id + " quer comer " + comidaConsumidaNessaRefeicao + " unidades de comida." + "\u001B[0m");
+                comedouro.comer(this, comidaConsumidaNessaRefeicao);
+                Thread.yield();
+                
+                // O animal para de comer e vai dormir
+                System.out.println(this.cor  + this.especie + " " + this.id + " terminou de comer " + comidaConsumidaNessaRefeicao + " unidades de comida." + "\u001B[0m");
+                int tempoDeSono = rand.nextInt(this.tempoDeSonoMax - this.tempoDeSonoMin + 1) + this.tempoDeSonoMin;
+                System.out.println(this.cor  + this.especie + " " + this.id + " vai dormir por " + tempoDeSono + " horas (segundos)." + "\u001B[0m");
+                Thread.sleep(1000 * tempoDeSono);
+                this.horasDormidas += tempoDeSono;
+
+                Thread.sleep((1000 * 24) - (1000 * tempoDeSono)); // Passa um dia
+
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
     }
-    
 }
--- Suricato.java
+++ Suricato.java
@@ -1,6 +1,5 @@
 class Suricato extends Animal {
 
-
     Suricato(Comedouro comedouro, int id){
         super(comedouro, id);
         this.especie = "Suricato";
@@ -8,42 +7,6 @@ class Suricato extends Animal {
         this.tempoDeSonoMax = 10;
         this.consomeMin = 1;
         this.consomeMax = 2;
-        
-    }
-
-    public void run() {
-        while(true){
-            try {
-                // O animal começa a comer
-                int comidaConsumidaNessaRefeicao = rand.nextInt(this.consomeMax - this.consomeMin + 1) + this.consomeMin;
-                System.out.println("\u001B[33m" + this.especie + " " + this.id + " quer comer " + comidaConsumidaNessaRefeicao + " unidades de comida." + "\u001B[0m");
-                comedouro.comer(this, comidaConsumidaNessaRefeicao);
-                Thread.yield();
-
-                // O animal para de comer e se exibe
-                System.out.println("\u001B[33m" + this.especie + " " + this.id + " terminou de comer " + comidaConsumidaNessaRefeicao + " unidades de comida." + "\u001B[0m");
-                this.comidaConsumida += comidaConsumidaNessaRefeicao;
-                System.out.println("\u001B[33m" + this.especie + " " + this.id + " começou a se exibir." + "\u001B[0m");
-
-                // O animal começa a comer de novo
-                comidaConsumidaNessaRefeicao = rand.nextInt(this.consomeMax - this.consomeMin + 1) + this.consomeMin;
-                this.comidaConsumida += comidaConsumidaNessaRefeicao;
-                System.out.println("\u001B[33m" + this.especie + " " + this.id + " quer comer " + comidaConsumidaNessaRefeicao + " unidades de comida." + "\u001B[0m");
-                comedouro.comer(this, comidaConsumidaNessaRefeicao);
-                Thread.yield();
-                
-                // O animal para de comer e vai dormir
-                System.out.println("\u001B[33m" + this.especie + " " + this.id + " terminou de comer " + comidaConsumidaNessaRefeicao + " unidades de comida." + "\u001B[0m");
-                int tempoDeSono = rand.nextInt(this.tempoDeSonoMax - this.tempoDeSonoMin + 1) + this.tempoDeSonoMin;
-                System.out.println("\u001B[33m" + this.especie + " " + this.id + " vai dormir por " + tempoDeSono + " horas (segundos)." + "\u001B[0m");
-                Thread.sleep(1000 * tempoDeSono);
-                this.horasDormidas += tempoDeSono;
-
-                Thread.sleep((1000 * 24) - (1000 * tempoDeSono)); // Passa um dia
-
-            } catch (InterruptedException e) {
-                return;
-            }
-        }
+        this.cor = "\u001B[33m";
     }
 }
